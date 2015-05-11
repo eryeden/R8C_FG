aa410281b64034b1797967466a06f70d3372437a
kazuki kikuti <ery.eden@gmail.com>
2015-05-11 09:57:20 +0900

    added : clock, Wave, Analog outputs, Digital outputs, TIMER, SSU, ADC, FG

--- sources/FG/FG.hpp
+++ sources/FG/FG.hpp
@@ -1,29 +1,106 @@
 #ifndef __FG__
 #define __FG__
 
+#define SLOT_SIZE_MAX 5
 
-class FG : public ADC{
-  
-  
-  
-  
-}; 
 
-class ASine : public Wave{};
-class ATriangle : public Wave{};
-class ASawtooth : public Wave{};
-class ANoise : public Wave{};
-class APWM : public Wave{};
+class Clock{
+private:
+  unsigned int nop;
+  float dt;
+  float pinv;
+  unsigned int f;
+  unsigned int k;
+  float pha;
+
+public:
+  void set_freq(unsigned int fin);
+  void set_dt(float dtin);
+  void set_nop(unsigned int nopin);
+  void set_phase(unsigned int phain);
+
+  unsigned int update();
+  
+};
+class Wave : public Clock{
+private:
+  float A;
+public:
+  
+  void set_gain(float gain);
+  float get_gain();
+
+  virtual unsigned int get_val_now();
+  unsigned int Out();
+
+  
+  void Enable();
+  void Disable();
+  
+};
+
+class ASine : public Wave{
+private:
+  
+public:
+  ASine(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+
+  unsigned int Out();
+};
+class ATriangle : public Wave{
+
+public:
+  ATriangle(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+
+  unsigned int Out();
+};
+class ASawtooth : public Wave{
+public:
+  ASawtooth(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+
+  unsigned int Out();
+};
+class ANoise : public Wave{
+public:
+  ANoise(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+
+  unsigned int Out();
+};
+class APWM : public Wave{
+public:
+  APWM(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+
+  unsigned int Out();
+};
 
 class DPWM{};
 class DNoise{};
 
-class Wave : public Clock{};
+class TIMER{
+  
+};
 
-class Clock{};
+class SSU{
+  
+};
+class ADC: public SSU{
+  
+};
 
-class ADC: public SSU{};
-class SSU{};
+class FG : public ADC ,public TIMER{
+private:
+  Wave slot[SLOT_SIZE_MAX];
+
+public:
+  FG();
+  
+  volatile void Update();
+  
+  FG& operator+=(Wave& w);
+  
+}; 
+
+
 
 
 #endif //__FG__ 
