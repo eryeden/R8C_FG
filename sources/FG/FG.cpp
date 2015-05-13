ab64f4b8f1f75b52452c60fd25ee7354034c7ff3
kazuki_kikuti <ery.eden@gmail.com>
2015-05-13 13:08:40 +0900

    add : BWave, AWave, Wave

--- sources/FG/FG.cpp
+++ sources/FG/FG.cpp
@@ -1,2 +1,1 @@
 #include "FG.hpp"
-#include <iostream>
--- sources/FG/FG.hpp
+++ sources/FG/FG.hpp
@@ -1,171 +1,184 @@
 #ifndef __FG__
 #define __FG__
 
-#define SLOT_SIZE_MAX 5
 
 
-
-class TIMER{
+class Timer{
 private:
 
 public:
-  void set_frequency(unsigned int fin);
-  void Enable();
-  void Disable();
+	void set_frequency(unsigned int fin);
+	void Enable();
+	void Disable();
 };
 
 
 
-class SSU{
+class SSUbus{
 
 public:
-  void write_byte(unsigned char data);
-  void write(unsigned char * data, unsigned char bytes);
+	void write_byte(unsigned char data);
+	void write(unsigned char * data, unsigned char bytes);
 };
 
-class ADC{
+class Dac{
 private:
-  SSU ssu;
+	SSUbus ssu;
 public:
-  void write(unsigned int vol);
+	void write(unsigned int vol);
 };
 
 class Clock{
 private:
-  unsigned int nop;
-  float dt;
-  float pinv;
-  unsigned int f;
-  unsigned int k;
-  float pha;
+	unsigned int nop;
+	float dt;
+	float pinv;
+	unsigned int f;
+	unsigned int k;
+	float pha;
 
 public:
-  void set_freq(unsigned int fin);
-  void set_dt(float dtin);
-  void set_nop(unsigned int nopin);
-  void set_phase(unsigned int phain);
+	void set_freq(unsigned int fin);
+	void set_dt(float dtin);
+	void set_nop(unsigned int nopin);
+	void set_phase(unsigned int phain);
 
-  unsigned int update();
+	unsigned int update();
 
 };
 
 class Wave{
 private:
-  float A;
-  Clock clk;
+
+
 public:
+	void set_frequency(unsigned int fin);
+	unsigned int get_frequency();
 
-  void set_gain(float gain);
-  float get_gain();
+	virtual unsigned int get_val_now();
+	unsigned int Out();
 
-  void set_frequency(unsigned int fin);
-  unsigned int get_frequency();
+	void Enable();
+	void Disable();
 
-  void set_phase(float phain);
-  float set_phase();
-
-  virtual unsigned int get_val_now();
-  unsigned int Out();
-
-  void Enable();
-  void Disable();
-
-  +virtual unsigned char get_id();
+	virtual unsigned char get_id();
 
 };
 
-
-class ANone : public Wave{
-
+class AWave : public Wave{
+private:
+	float m_A;
+protected:
+	Clock m_clk;
 public:
-  ANone(unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	void set_frequency(unsigned int fin);
+	unsigned int get_frequency();
+
+	void set_gain(float gain);
+	float get_gain();
+
+	void set_phase(float phain);
+	float set_phase();
 };
 
-class ASine : public Wave{
+class ANone : public AWave{
+
+public:
+	unsigned int get_val_now();
+};
+
+class ASine : public AWave{
 private:
 
 public:
-  ASine(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
+	ASine(const unsigned int* stale, unsigned int nop, unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	unsigned int get_val_now();
 };
-class ATriangle : public Wave{
+class ATriangle : public AWave{
 
 public:
-  ATriangle(unsigned int freq, unsigned int phase, float gain);
+	ATriangle(unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	unsigned int get_val_now();
 };
-class ASawtooth : public Wave{
+class ASawtooth : public AWave{
 public:
-  ASawtooth(unsigned int freq, unsigned int phase, float gain);
+	ASawtooth(unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	unsigned int get_val_now();
 };
-class ANoise : public Wave{
+class ANoise : public AWave{
 public:
-  ANoise(unsigned int freq, unsigned int phase, float gain);
+	ANoise(unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	unsigned int get_val_now();
 };
-class APWM : public Wave{
+class APWM : public AWave{
 public:
-  APWM(unsigned int freq, unsigned int phase, float gain);
+	APWM(unsigned int freq, unsigned int phase, float gain);
 
-  unsigned int get_val_now();
+	unsigned int get_val_now();
 };
 
 
-class BWave{
+class BWave : public Wave{
+public:
 
 public:
-  void set_frequency(unsigned int fin);
-  void set_duty(unsigned int duty);
-  virtual unsigned int Out();
-
+	void set_frequency(unsigned int fin);
+	void set_duty(unsigned int duty);
 };
 
 class BPWM : public BWave{
 private:
-  //TIMER tim;
+
 public:
-  unsigned int Out();
+	unsigned int get_val_now();
 };
 class BNoise : public BWave{
 private:
-  //TIMER tim;
+
 public:
-  unsigned int Out();
+	unsigned int get_val_now();
 };
 
-class FG{
+
+
+class Functiongenerator{
 private:
-  ADC adc;
-  TIMER tim;
-  Wave slot[SLOT_SIZE_MAX];
-  BWave *bslot;
-  unsigned int nos;
-  void AUpdate();
-  void BUpdate();
+
+	static const unsigned char SLOT_SIZE_MAX = 5;
+	Dac dac;
+	Timer tim;
+	Wave slot[SLOT_SIZE_MAX];
+	BWave *bslot;
+	void AUpdate();
+	void BUpdate();
 
 
 
 public:
-  FG();
-  void clear();
-  volatile void Update();
+	Functiongenerator(); //ここですべてのスロットにANone, BNoneを入れておく。
+	void clear();
+	void Update();
 
-  FG& operator+=(Wave& w);
-
-  void insertwave(unsigned char idx, Wave& w);
-  void insertwave(unsigned char idx, BWave& w);
-  void deletewave(unsigned char idx);
+	void insertwave(unsigned char idx, AWave& w);
+	void insertwave(unsigned char idx, BWave& w);
+	void deletewave(unsigned char idx);
 
 };
 
 
+class LCD_Utils{};
+class UI_Utils{};
+class FGManeger{
+private:
+
+public:
+
+};
 
 
 #endif //__FG__
